dd795686cd342b0d6948e4d8e883b168f8094c16
jiangzhuofan <shaonaiyi@163.com>
2022-02-22 16:52:09 +0800

    hot-fix commit1

--- src/main/java/com/ryit/test/Test.java
+++ src/main/java/com/ryit/test/Test.java
@@ -5,5 +5,6 @@ public class Test {
         System.out.println("Hello world");
         System.out.println("Hello world2");
         System.out.println("Hello world2");
+        System.out.println("Hello world3");
     }
 }
